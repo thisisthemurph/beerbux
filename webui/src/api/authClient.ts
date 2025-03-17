bec2ff526be54c0afdcddf38ce0b3a76404ff3d3
Mike Murphy <michael.w.murphy@outlook.com>
2025-03-17 05:55:22 +0000

    Sets base url as env var

--- webui/src/api/authClient.ts
+++ webui/src/api/authClient.ts
@@ -8,13 +8,15 @@ type LoginResponse = {
 	username: string;
 };
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 function useAuthClient() {
 	async function login(
 		username: string,
 		password: string,
 	): Promise<LoginResponse | ValidationErrorResponse> {
 		try {
-			const response = await fetch("http://localhost:42069/auth/login", {
+			const response = await fetch(`${API_BASE_URL}/auth/login`, {
 				method: "POST",
 				credentials: "include",
 				body: JSON.stringify({ username, password }),
@@ -50,7 +52,7 @@ function useAuthClient() {
 		verificationPassword: string,
 	): Promise<ValidationErrorResponse | undefined> {
 		try {
-			const response = await fetch("http://localhost:42069/auth/signup", {
+			const response = await fetch(`${API_BASE_URL}/auth/signup`, {
 				method: "POST",
 				credentials: "include",
 				body: JSON.stringify({

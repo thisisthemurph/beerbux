f3bbb77362c0aeb99870ee0157c941abd0c64e5a
Mike Murphy <michael.w.murphy@outlook.com>
2025-03-31 08:24:18 +0100

    Current member always on top

--- webui/src/features/session/detail/member-details-card.tsx
+++ webui/src/features/session/detail/member-details-card.tsx
@@ -8,6 +8,7 @@ import {
 import { UserAvatar } from "@/components/user-avatar.tsx";
 import type { AvatarData } from "@/hooks/user-avatar-data.ts";
 import { cn } from "@/lib/utils";
+import { useUserStore } from "@/stores/user-store.tsx";
 
 type MemberDetailsCardProps = {
 	members: SessionMember[];
@@ -18,21 +19,39 @@ export function MemberDetailsCard({
 	members,
 	avatarData,
 }: MemberDetailsCardProps) {
+	const user = useUserStore((state) => state.user);
+	const currentMember = members.find((member) => member.id === user?.id);
+
 	return (
 		<Card>
 			<CardHeader>
 				<CardTitle>Members</CardTitle>
 			</CardHeader>
 			<CardContent>
-				{members.map(({ id, name, username, transactionSummary }) => (
-					<div key={id} className="flex items-center gap-4">
-						<UserAvatar data={avatarData[username]} tooltip={name} />
+				{currentMember && (
+					<div className="flex items-center gap-4">
+						<UserAvatar
+							data={avatarData[currentMember.username]}
+							tooltip={currentMember.name}
+						/>
 						<div className="flex justify-between items-center w-full">
-							<p className="py-6 font-semibold">{username}</p>
-							<Balance {...transactionSummary} />
+							<p className="py-6 font-semibold">{currentMember.username}</p>
+							<Balance {...currentMember.transactionSummary} />
 						</div>
 					</div>
-				))}
+				)}
+				{members
+					.filter((m) => m.id !== currentMember?.id)
+					.sort((a, b) => a.name.localeCompare(b.name))
+					.map(({ id, name, username, transactionSummary }) => (
+						<div key={id} className="flex items-center gap-4">
+							<UserAvatar data={avatarData[username]} tooltip={name} />
+							<div className="flex justify-between items-center w-full">
+								<p className="py-6 font-semibold">{username}</p>
+								<Balance {...transactionSummary} />
+							</div>
+						</div>
+					))}
 			</CardContent>
 		</Card>
 	);

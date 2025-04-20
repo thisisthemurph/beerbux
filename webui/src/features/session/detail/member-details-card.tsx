797b1f87bf54343ad3a9a2447cb952ea96c81071
Mike Murphy <michael.w.murphy@outlook.com>
2025-04-20 13:39:34 +0100

    Admin UI considerations

--- webui/src/features/session/detail/member-details-card.tsx
+++ webui/src/features/session/detail/member-details-card.tsx
@@ -10,17 +10,33 @@ import { UserAvatar } from "@/components/user-avatar.tsx";
 import { useInformationDialog } from "@/hooks/use-information-dialog.tsx";
 import type { AvatarData } from "@/hooks/user-avatar-data.ts";
 import { cn } from "@/lib/utils";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {Crown, EllipsisVertical, ShieldMinus, ShieldUser, UserMinus} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
+import {useUserStore} from "@/stores/user-store.tsx";
 
 type MemberDetailsCardProps = {
 	members: SessionMember[];
 	avatarData: Record<string, AvatarData>;
+	showMemberDropdownMenu: boolean;
+	onChangeMemberAdminState: (memberId: string, newAdminState: boolean) => void;
 };
 
 export function MemberDetailsCard({
 	members,
 	avatarData,
+	showMemberDropdownMenu,
+	onChangeMemberAdminState,
 }: MemberDetailsCardProps) {
 	const [openInformationDialog, InformationDialog] = useInformationDialog();
+	const user = useUserStore((state) => state.user);
 
 	const handleInformationClick = () => {
 		openInformationDialog({
@@ -40,13 +56,24 @@ export function MemberDetailsCard({
 						<InformationButton onClick={handleInformationClick} />
 					</section>
 				</CardHeader>
-				<CardContent>
-					{members.map(({ id, name, username, transactionSummary }) => (
-						<div key={id} className="flex items-center gap-4">
-							<UserAvatar data={avatarData[username]} tooltip={name} />
+				<CardContent className="px-0">
+					{members.map((m) => (
+						<div key={m.id} className="group flex items-center gap-4 px-6 hover:bg-muted">
+							<UserAvatar data={avatarData[m.username]} tooltip={m.name} variant={m.isAdmin ? "prominent" : "default"} />
 							<div className="flex justify-between items-center w-full">
-								<p className="py-6 font-semibold">{username}</p>
-								<Balance {...transactionSummary} />
+								<button type="button" className="flex items-center gap-2 py-6 font-semibold">
+									<span>{m.username}</span>
+								</button>
+								<div className={cn("", showMemberDropdownMenu && "w-16 text-left grid grid-cols-2")}>
+									<Balance {...m.transactionSummary} />
+									{(m.id !== user?.id && showMemberDropdownMenu) && (
+										<MemberDropdownMenu
+											username={m.username}
+											isAdmin={m.isAdmin}
+											onChangeAdminState={() => onChangeMemberAdminState(m.id, !m.isAdmin)}
+										/>
+									)}
+								</div>
 							</div>
 						</div>
 					))}
@@ -56,13 +83,54 @@ export function MemberDetailsCard({
 	);
 }
 
+type MemberDropdownMenuProps = {
+	username: string;
+	isAdmin: boolean;
+	onChangeAdminState: () => void;
+}
+
+function MemberDropdownMenu({ username, isAdmin, onChangeAdminState }: MemberDropdownMenuProps) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger className="py-6 font-semibold w-full text-left" asChild>
+				<Button variant="ghost" size="icon" className="rounded-full">
+					<EllipsisVertical className="size-6 text-muted-foreground group-hover:text-primary" />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="min-w-46 mx-4" onClick={onChangeAdminState}>
+				<DropdownMenuLabel className="flex items-center justify-between font-semibold text-lg">
+					<span>{username}</span>
+					{isAdmin && <span><Crown className="size-4" /></span>}
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				{!isAdmin && (
+					<DropdownMenuItem className="text-lg gap-4 cursor-pointer" onClick={onChangeAdminState}>
+						<ShieldUser className="size-6"/>
+						<span>Promote to admin</span>
+					</DropdownMenuItem>
+				)}
+				{isAdmin && (
+					<DropdownMenuItem className="text-lg gap-4 cursor-pointer">
+						<ShieldMinus className="size-6" />
+						<span>Remove admin</span>
+					</DropdownMenuItem>
+				)}
+				<DropdownMenuItem className="text-lg gap-4 cursor-pointer">
+					<UserMinus className="size-6" />
+					<span>Remove from session</span>
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	)
+}
+
 function Balance({ credit, debit }: { credit: number; debit: number }) {
 	const value = credit - debit;
 
 	return (
 		<p
 			className={cn(
-				"font-semibold text-muted-foreground",
+				"font-semibold text-muted-foreground text-lg flex items-center",
 				value > 0 && "text-green-600",
 				value < 0 && "text-red-600",
 			)}

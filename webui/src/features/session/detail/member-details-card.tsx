a9fe2c1a90cce09d25f20c269fec03e755a4c628
Mike Murphy <michael.w.murphy@outlook.com>
2025-04-21 08:12:39 +0100

    Adds confirmation for removing a member

--- webui/src/features/session/detail/member-details-card.tsx
+++ webui/src/features/session/detail/member-details-card.tsx
@@ -10,23 +10,8 @@ import { UserAvatar } from "@/components/user-avatar.tsx";
 import { useInformationDialog } from "@/hooks/use-information-dialog.tsx";
 import type { AvatarData } from "@/hooks/user-avatar-data.ts";
 import { cn } from "@/lib/utils";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-	Crown,
-	EllipsisVertical,
-	ShieldMinus,
-	ShieldUser,
-	UserMinus,
-} from "lucide-react";
-import { Button } from "@/components/ui/button.tsx";
 import { useUserStore } from "@/stores/user-store.tsx";
+import { MemberDropdownMenu } from "./member-dropdown-menu";
 
 type MemberDetailsCardProps = {
 	members: SessionMember[];
@@ -106,62 +91,6 @@ export function MemberDetailsCard({
 				</CardContent>
 			</Card>
 		</>
-	);
-}
-
-type MemberDropdownMenuProps = {
-	username: string;
-	isAdmin: boolean;
-	onChangeAdminState: () => void;
-	onRemoveMember: () => void;
-};
-
-function MemberDropdownMenu({
-	username,
-	isAdmin,
-	onChangeAdminState,
-	onRemoveMember,
-}: MemberDropdownMenuProps) {
-	return (
-		<DropdownMenu>
-			<DropdownMenuTrigger
-				className="py-6 font-semibold w-full text-left"
-				asChild
-			>
-				<Button variant="ghost" size="icon" className="rounded-full">
-					<EllipsisVertical className="size-6 text-muted-foreground group-hover:text-primary" />
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent className="min-w-46 mx-4">
-				<DropdownMenuLabel className="flex items-center justify-between font-semibold text-lg">
-					<span>{username}</span>
-					{isAdmin && (
-						<span>
-							<Crown className="size-4" />
-						</span>
-					)}
-				</DropdownMenuLabel>
-				<DropdownMenuSeparator />
-				<DropdownMenuItem
-					className="text-lg gap-4 cursor-pointer"
-					onClick={onChangeAdminState}
-				>
-					{isAdmin ? (
-						<ShieldMinus className="size-6" />
-					) : (
-						<ShieldUser className="size-6" />
-					)}
-					<span>{isAdmin ? "Remove admin" : "Promote to admin"}</span>
-				</DropdownMenuItem>
-				<DropdownMenuItem
-					className="text-lg gap-4 cursor-pointer"
-					onClick={onRemoveMember}
-				>
-					<UserMinus className="size-6" />
-					<span>Remove from session</span>
-				</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
 	);
 }
 
---
+++ webui/src/features/session/detail/member-dropdown-menu.tsx
@@ -0,0 +1,88 @@
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import {
+	Crown,
+	EllipsisVertical,
+	ShieldMinus,
+	ShieldUser,
+	UserMinus,
+} from "lucide-react";
+import {useConfirmationDialog} from "@/hooks/use-confirmation-dialog.tsx";
+
+type MemberDropdownMenuProps = {
+	username: string;
+	isAdmin: boolean;
+	onChangeAdminState: () => void;
+	onRemoveMember: () => void;
+};
+
+export function MemberDropdownMenu({
+	username,
+	isAdmin,
+	onChangeAdminState,
+	onRemoveMember,
+}: MemberDropdownMenuProps) {
+	const [openConfirmationDialog, ConfirmationDialog] = useConfirmationDialog();
+
+	const handleRemoveMember = (username: string) => {
+		openConfirmationDialog({
+			title: `Are you sure you want to remove ${username}?`,
+			description: `Removing ${username} will not remove any of the history associated with them, but they will no longer be able to access this session.`,
+			confirmText: "Remove member",
+			cancelText: "Cancel",
+			onConfirm: onRemoveMember,
+		});
+	};
+
+	return (
+		<>
+			<ConfirmationDialog />
+			<DropdownMenu>
+				<DropdownMenuTrigger
+					className="py-6 font-semibold w-full text-left"
+					asChild
+				>
+					<Button variant="ghost" size="icon" className="rounded-full">
+						<EllipsisVertical className="size-6 text-muted-foreground group-hover:text-primary" />
+					</Button>
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className="min-w-46 mx-4">
+					<DropdownMenuLabel className="flex items-center justify-between font-semibold text-lg">
+						<span>{username}</span>
+						{isAdmin && (
+							<span>
+								<Crown className="size-4" />
+							</span>
+						)}
+					</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuItem
+						className="text-lg gap-4 cursor-pointer"
+						onClick={onChangeAdminState}
+					>
+						{isAdmin ? (
+							<ShieldMinus className="size-6" />
+						) : (
+							<ShieldUser className="size-6" />
+						)}
+						<span>{isAdmin ? "Remove admin" : "Promote to admin"}</span>
+					</DropdownMenuItem>
+					<DropdownMenuItem
+						className="text-lg gap-4 cursor-pointer"
+						onClick={() => handleRemoveMember(username)}
+					>
+						<UserMinus className="size-6" />
+						<span>Remove from session</span>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
+		</>
+	);
+}

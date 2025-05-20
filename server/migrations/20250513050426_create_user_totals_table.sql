a1ce438c72572db0e4fbb8183a177f015fe3977d
Mike Murphy <michael.w.murphy@outlook.com>
2025-05-20 07:38:50 +0100

    Fix issues with numeric format

--- server/migrations/20250513050426_create_user_totals_table.sql
+++ server/migrations/20250513050426_create_user_totals_table.sql
@@ -2,8 +2,8 @@
 -- +goose StatementBegin
 create table if not exists user_totals (
     user_id uuid references users(id) on delete cascade,
-    credit numeric(2,1) not null default 0,
-    debit numeric(2,1) not null default 0,
+    credit numeric(5,1) not null default 0,
+    debit numeric(5,1) not null default 0,
     primary key (user_id)
 );
 -- +goose StatementEnd
--- server/migrations/20250513054624_carete_ledger_table.sql
+++ server/migrations/20250513054624_carete_ledger_table.sql
@@ -4,7 +4,7 @@ create table if not exists ledger (
     id uuid primary key default uuid_generate_v4(),
     transaction_id uuid not null references session_transactions(id) on delete no action,
     user_id uuid not null references users(id) on delete no action,
-    amount numeric(2,1) not null,
+    amount numeric(5,1) not null,
     created_at timestamp with time zone not null default now()
 );
 -- +goose StatementEnd

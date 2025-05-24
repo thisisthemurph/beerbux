d39751dfc98db8e27aeb0f6734f26fec8a9fced1
Mike Murphy <michael.w.murphy@outlook.com>
2025-05-24 11:22:17 +0100

    Add envars to build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 cd webui || exit
 
+export VITE_API_BASE_URL=/api
+export VITE_SSE_BASE_URL=/api/events
+export VITE_TEST="this is only a test"
+
+echo "API_BASE_URL: $VITE_API_BASE_URL"
+
 npm ci
 npm run build
 
